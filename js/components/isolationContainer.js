9973e66b076d902cdce1b7e21921062caebe9bea
Kory Becker <kbecker@primaryobjects.com>
2020-01-01 20:35:59 -0500

    Create isolationContainer.js

---
+++ js/components/isolationContainer.js
@@ -0,0 +1,40 @@
+class IsolationContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      strategy: props.strategy,
+    };
+    
+    this.onType = this.onType.bind(this);
+  }
+  
+  onType(e) {
+    let strategy;
+    
+    switch (e.currentTarget.value) {
+      case 'random': strategy = StrategyManager.random; break;
+      case 'minimax': strategy = StrategyManager.minimax; break;
+      case 'none': strategy = StrategyManager.none; break;
+    };
+    
+    this.setState({ strategy });
+    
+    console.log(`Strategy set to ${e.currentTarget.value}.`);
+  }
+  
+  render() {
+    return (
+      <div>
+        <Isolation width={ this.props.width } height={ this.props.height } strategy={ this.state.strategy }></Isolation>
+          
+        <div class="gamePlayOptions">
+          <span>Game Play</span>
+          <input type="radio" name="type" value="random" checked={this.state.strategy === StrategyManager.random} onChange={ this.onType }/> <span>Random</span>
+          <input type="radio" name="type" value="minimax" checked={this.state.strategy === StrategyManager.minimax} onChange={ this.onType }/> <span>Minimax</span>
+          <input type="radio" name="type" value="none" checked={!this.state.strategy || this.state.strategy === StrategyManager.none} onChange={ this.onType }/> <span>2 Players</span>
+        </div>
+      </div>
+    );
+  }
+}
